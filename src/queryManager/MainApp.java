8b79dd388124f4ff1f1978af62688bb732fd277f
Andres Lopez <andoresu47@users.noreply.github.com>
2015-10-23 21:39:12 -0500

    Added functionality by reading data from database and initializing the view based on it.

--- src/queryManager/MainApp.java
+++ src/queryManager/MainApp.java
@@ -4,28 +4,31 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import queryManager.view.Controller;
 
+import java.io.IOException;
 import java.sql.*;
 
 public class MainApp extends Application {
 
+    private Stage primaryStage;
     private Connection connection = null;
 
     /**
      * Constructor which initializes the connection to the database.
      */
-    public MainApp(){
+   /* public MainApp(){
         try{
             // create a database connection
             this.connection = DriverManager.getConnection("jdbc:sqlite:.\\src\\queryManager\\database\\database.db");
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);  // set timeout to 30 sec.
+            System.out.println("Connection to database: successful");
 
         }catch(SQLException e){
             System.err.println(e.getMessage());
         }
-    }
+    }*/
 
     /**
      * Gets executed as the application starts.
@@ -34,16 +37,33 @@ public class MainApp extends Application {
      */
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/layout.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 640, 480));
-        primaryStage.show();
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Hello World");
+
+        initLayout();
     }
 
     /**
-     * Main method.
-     * @param args
+     * Method that manages the loading of the fxml file and overall
+     * initialization of the application.
      */
+    public void initLayout(){
+        try{
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/layout.fxml"));
+            BorderPane rootLayout = (BorderPane) loader.load();
+
+            //Controller controller = loader.getController();
+
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
--- src/queryManager/view/Controller.java
+++ src/queryManager/view/Controller.java
@@ -1,13 +1,105 @@
 package queryManager.view;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBoxTreeItem;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TreeView;
+import javafx.scene.control.cell.CheckBoxTreeCell;
 import queryManager.MainApp;
 
-import javax.swing.text.TableView;
-import java.sql.Statement;
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
 
-public class Controller {
+public class Controller implements Initializable{
 
+    @FXML
     private TableView table;
+
+    @FXML
+    private TreeView<String> authorsTree;
+
+    @FXML
+    private TreeView<String> booksTree;
+
+    @FXML
+    private TreeView<String> publishersTree;
+
+    @FXML
+    private TreeView<String> yearsTree;
+
+    @FXML
+    private TreeView<String> genresTree;
+
     private Statement statement;
     private MainApp mainApp;
+
+    public void setStatement(Statement statement){
+        this.statement = statement;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        try{
+            // create a database connection
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:.\\src\\queryManager\\database\\database.db");
+            System.out.println("Connection to database: successful");
+            this.statement = connection.createStatement();
+            this.statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+        }catch(SQLException e){
+            System.err.println(e.getMessage());
+        }
+
+        initializeAuthorsTree();
+        //initializeBooksTree();
+        //initializePublishersTree();
+        //initializeYearsTree();
+        //initializeGenresTree();
+    }
+
+    public void initializeAuthorsTree(){
+        CheckBoxTreeItem<String> rootItem = new CheckBoxTreeItem<>("Authors");
+        rootItem.setExpanded(true);
+
+        authorsTree.setRoot(rootItem);
+        authorsTree.setEditable(true);
+
+        authorsTree.setCellFactory(CheckBoxTreeCell.forTreeView());
+
+        try{
+            ResultSet rs = statement.executeQuery("select Author from Authors");
+            while(rs.next()){
+                CheckBoxTreeItem<String> checkBoxTreeItem = new CheckBoxTreeItem<>(rs.getString("Author"));
+
+                checkBoxTreeItem.selectedProperty().addListener((observable, oldValue, newValue) -> {
+                    if(newValue){
+                        System.out.println("The selected item is " + checkBoxTreeItem.valueProperty().get());
+                    }
+                });
+                rootItem.getChildren().add(checkBoxTreeItem);
+            }
+
+        }catch(SQLException e){
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void initializeBooksTree(){
+
+    }
+
+    public void initializePublishersTree(){
+
+    }
+
+    public void initializeYearsTree(){
+
+    }
+
+    public void initializeGenresTree(){
+
+    }
 }
